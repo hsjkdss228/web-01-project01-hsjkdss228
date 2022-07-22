3d06b617e7690bca18a3c51e5c39543eb4c2839d
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-22 12:37:09 +0900

    ExercisePlanPost에서 더 이상 사용되지 않는 데이터에 대한 테스트 제거

---
+++ app/src/test/java/models/ExerciseRecordPostTest.java
@@ -0,0 +1,13 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExerciseRecordPostTest {
+  @Test
+  void creation() {
+
+  }
+
+}
